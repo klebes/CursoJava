2c863d6d52477cc4d7acb47383ee9f6d32063624
klebes <klebes-santos@blogdokdsti.com.br>
2022-07-10 19:01:04 -0300

    classe pessoa like

--- src/bancodados/ConsultaPessoaLike.java
+++ src/bancodados/ConsultaPessoaLike.java
@@ -5,6 +5,7 @@
 package bancodados;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -17,26 +18,29 @@ import java.util.Scanner;
  * @author KDS
  */
 public class ConsultaPessoaLike {
+
     public static void main(String[] args) throws SQLException {
         Scanner teclado = new Scanner(System.in);
-        String sql = "SELECT * FROM pessoa where nome like '%kl%'";
-        
         Connection conexao = FabricaConnection.geConexao();
-        Statement stmt = conexao.createStatement();
-        ResultSet resultado = stmt.executeQuery(sql);
-        
+        String sql = "SELECT * FROM pessoa where nome like ?";
+        System.out.println("digite um nome: ");
+        String valor = teclado.nextLine();
+
+        PreparedStatement stmt = conexao.prepareStatement(sql);
+        stmt.setString(1, "%" + valor + "%");
+        ResultSet resultado = stmt.executeQuery();
+
         List<Pessoa> pessoa = new ArrayList<>();
         while (resultado.next()) {
             int codigo = resultado.getInt("codigo");
             String nome = resultado.getString("nome");
             pessoa.add(new Pessoa(codigo, nome));
         }
-        for(Pessoa p : pessoa){
-            System.out.println("usuario" +p.getNumber() + p.getNome());
-            
-           
+        for (Pessoa p : pessoa) {
+            System.out.println("usuario" + p.getNumber() + p.getNome());
+
         }
         stmt.close();
-         teclado.close();
+        teclado.close();
     }
 }
